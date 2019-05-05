aac18796d19e7ca87667518412df150d7b3ee628
dicaminha <dineresc@gmail.com>
2019-05-05 12:12:27 -0300

    de

--- src/app/pages/task/task-list/task-list-grid-view/task-list-grid-view.component.ts
+++ src/app/pages/task/task-list/task-list-grid-view/task-list-grid-view.component.ts
@@ -1,10 +1,5 @@
-import {Component, Input, OnInit} from "@angular/core";
+import { Component, Input } from "@angular/core";
 
-import { columnTaskDefinitions, localeText } from "../../../../shared/components/gt-grid/gt-grid-definitions";
-import { TaskService } from "../../../../core/services/task.service";
-import { GladService } from "../../../../core/services/glad.service";
-import { SharedService } from "../../../../core/services/shared.service";
-import { Router } from "@angular/router";
 import { Task } from "../../../../shared/models/task.model";
 
 @Component({
@@ -12,40 +7,9 @@ import { Task } from "../../../../shared/models/task.model";
 	templateUrl: './task-list-grid-view.component.html',
 	styleUrls: ['./task-list-grid-view.component.scss']
 })
-export class TaskListGridViewComponent implements OnInit {
+export class TaskListGridViewComponent {
 	@Input('tasksDueToday') tasksDueToday: Task[];
 	@Input('tasksNotDueToday') tasksNotDueToday: Task[];
 	@Input('tasksDelayed') tasksDelayed: Task[];
 	@Input('tasksFinished') tasksFinished: Task[];
-
-	gridOptions: any = {};
-	localeText = localeText;
-	columnTaskDefinitions = columnTaskDefinitions;
-	
-	constructor(
-		private taskService: TaskService,
-		private gladService: GladService,
-		private sharedService: SharedService,
-		private router: Router) { }
-
-	ngOnInit(): void {
-		this.configureGridRowStyle();
-	}
-
-	configureGridRowStyle(): void {
-		this.gridOptions.getRowStyle = (params) => {
-			if (params.data.priority === 'Baixo') {
-				return null;
-			} else if (params.data.priority === 'Normal') {
-				return { background: 'yellow' };
-			} else {
-				return { background: 'red' };
-			}
-		};
-	}
-
-	onRowSelected(row):void {
-		let task = row.data;
-		this.router.navigate(['tasks', 'task-form', task.number]);
-	}
 }
