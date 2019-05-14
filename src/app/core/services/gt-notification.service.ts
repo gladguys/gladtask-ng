88ffc82059ae9c4451c6d7c159f3be36e9b2ca3d
Rodrigo <rodrigobastosv@gmail.com>
2019-05-13 23:03:58 -0300

    Adapting the service to use ngx toastr

---
+++ src/app/core/services/gt-notification.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+import { ToastrService } from "ngx-toastr";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GTNotificationService {
+
+	constructor(private toastr: ToastrService) {}
+
+	notificateSuccess(message: string) {
+		this.toastr.success(message, '', {
+			positionClass: 'toast-top-center'
+		});
+	}
+	
+	notificateFailure(message: string) {
+		this.toastr.error(message, '', {
+			positionClass: 'toast-top-center'
+		});
+	}
+	
+	notificateInfo(message: string) {
+		this.toastr.info(message, '', {
+			positionClass: 'toast-top-center'
+		});
+	}
+
+	notificateWarning(message: string) {
+		this.toastr.warning(message, '', {
+			positionClass: 'toast-top-center'
+		});
+	}
+}
