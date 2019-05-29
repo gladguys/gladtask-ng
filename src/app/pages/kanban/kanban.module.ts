01dc5e4032e8286d06fb2c230d3100c7a00be6e1
Alexandre <alexlima.crvg@gmail.com>
2019-05-28 21:41:23 -0300

    mais ajustes no kanban

--- src/app/pages/kanban/kanban.module.ts
+++ src/app/pages/kanban/kanban.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { KanbanComponent } from './kanban.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faExpand } from '@fortawesome/free-solid-svg-icons';
 
 import { CoreModule } from '../../core/core.module';
 import { SharedModule } from '../../shared/shared.module';
@@ -11,10 +14,15 @@ import { TaskModule } from '../task/task.module';
 	declarations: [KanbanComponent],
 	imports: [
 		FlexLayoutModule,
+		FontAwesomeModule,
 		CoreModule,
 		SharedModule,
 		TaskModule,
 		DragDropModule
 	]
 })
-export class KanbanModule { }
+export class KanbanModule {
+	constructor() {
+		library.add(faExpand);
+	}
+}
