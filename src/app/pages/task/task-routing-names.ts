c62ee552f7268521e1a85ccf902e58988e1a8f3c
Denis Caminha <dineresc@gmail.com>
2019-06-05 23:57:31 -0300

    Update task-routing-names.ts

--- src/app/pages/task/task-routing-names.ts
+++ src/app/pages/task/task-routing-names.ts
@@ -1,7 +1,7 @@
 export const TaskRoutingNames = {
     TASKS: 'tasks',
     TASK_FORM: 'task-form',
-    TASK_DETAIL: 'task_detail',
+    TASK_DETAIL: 'task-detail',
     TASK_NOT_FOUND: 'task-not-found'
 
 }
