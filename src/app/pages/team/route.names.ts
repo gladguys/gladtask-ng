b54350540e99d2a51320baeb7a117b332192f655
dicaminha <dineresc@gmail.com>
2019-06-08 00:36:46 -0300

    retirando status de signuo

---
+++ src/app/pages/team/route.names.ts
@@ -0,0 +1,5 @@
+export const TeamRoutesNames = {
+    TEAMS: 'teams',
+    TEAM_FORM: 'team-form',
+    TEAM_NOT_FOUND: 'team-not-found'
+}
---
+++ src/app/pages/user/route.names.ts
@@ -0,0 +1,3 @@
+export const UserRoutesNames = {
+    USERS: 'users'
+}
