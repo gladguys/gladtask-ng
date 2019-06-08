0ea87838f12cecd172f0da0a5cf6dcf0ac0b5d12
dicaminha <dineresc@gmail.com>
2019-06-08 13:46:53 -0300

    remove trash

--- src/app/pages/team/route.names.ts
+++
@@ -1,5 +0,0 @@
-export const TeamRoutesNames = {
-    TEAMS: 'teams',
-    TEAM_FORM: 'team-form',
-    TEAM_NOT_FOUND: 'team-not-found'
-}
--- src/app/pages/user/route.names.ts
+++
@@ -1,3 +0,0 @@
-export const UserRoutesNames = {
-    USERS: 'users'
-}
