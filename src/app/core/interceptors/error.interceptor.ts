72a4afdf7239069b398f25869399d59f938ecc41
Rodrigo <rodrigobastosv@gmail.com>
2019-06-19 01:29:40 -0300

    Adding errpr interceptor

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+	HttpErrorResponse,
+	HttpEvent,
+	HttpHandler,
+	HttpInterceptor,
+	HttpRequest,
+} from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { Observable, of } from "rxjs";
+import { GTNotificationService } from "../services/gt-notification.service";
+import { Injectable, Injector } from "@angular/core";
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+	
+	constructor(private injector: Injector) {
+	}
+	
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		const notificationService = this.injector.get(GTNotificationService);
+		
+		return next.handle(req).pipe(
+			catchError((err: any) => {
+				if (err instanceof HttpErrorResponse) {
+					console.log(err);
+					if (err.status === 400) {
+						notificationService.notificateFailure('Ocorreu um erro na requisição.')
+					} else if (err.status === 400) {
+						notificationService.notificateFailure('Ocorreu um erro interno.')
+					}
+				}
+				return of(err);
+			}));
+		
+	}
+}
