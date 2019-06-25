8f5b379a1b25b0a9f94044c6188913e50852b39b
Rodrigo <rodrigobastosv@gmail.com>
2019-06-24 22:14:45 -0300

    Autologout on guards

--- src/app/core/guards/team-form.guard.ts
+++ src/app/core/guards/team-form.guard.ts
@@ -5,13 +5,20 @@ import { Observable } from "rxjs";
 
 import { GTConfirmationDialogComponent } from "../../shared/components/gt-confirmation-dialog/gt-confirmation-dialog.component";
 import { TeamFormComponent } from 'src/app/pages/team/team-form/team-form.component';
+import { SharedService } from "../services/shared.service";
 
 @Injectable()
 export class TeamFormGuard implements CanDeactivate<TeamFormComponent> {
 
-	constructor(private matDialog: MatDialog) {}
+	constructor(
+		private matDialog: MatDialog,
+		private sharedService: SharedService) {}
 
 	canDeactivate(component: TeamFormComponent, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): Observable<boolean>  | boolean {
+		if (!this.sharedService.isUserLoggedIn()) {
+			return true;
+		}
+
         if (component.isDirty()) {
 			let dialogRef = this.matDialog.open(GTConfirmationDialogComponent, {
 				width: '400px'
