dd454937ff608d7af9d9b6e43873e62c28b68286
Denis Caminha <dineresc@gmail.com>
2019-07-15 02:03:02 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	VERSION: require('../../package.json').version,
 	production: true,
-	API: "http://34.67.235.246/api",
-	API_ADRESS: "http://34.67.235.246/api"
+	API: "http://34.67.235.246:8080/api",
+	API_ADRESS: "http://34.67.235.246:8080/api"
 };
