ca9e27b5944490b215846b805269949a9d83be62
Denis Caminha <dineresc@gmail.com>
2019-11-06 00:53:48 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	VERSION: require('../../package.json').version,
 	production: true,
-	API: "http://34.67.235.246/api",
-	API_ADRESS: "http://34.67.235.246/api"
+	API: "http://api-gladtask.herokuapp.com/api",
+	API_ADRESS: "http://api-gladtask.herokuapp.com/api"
 };
