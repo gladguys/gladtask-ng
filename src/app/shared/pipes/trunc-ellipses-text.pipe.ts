c9ff5cd17e36ce732345b7afab5ea47c4fac5058
Denis <dineresc@gmail.com>
2021-01-05 01:57:19 -0300

    add parameter limit Size of characters

--- src/app/shared/pipes/trunc-ellipses-text.pipe.ts
+++ src/app/shared/pipes/trunc-ellipses-text.pipe.ts
@@ -5,9 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TruncEllipsesTextPipe implements PipeTransform {
 
-    transform(text: string): string {
+    transform(text: string, arg1: number): string {
+        if (arg1 == null) arg1 = 15;
         if (text.length >= 15) 
-            return text.substring(0,15) + '...';
+            return text.substring(0,arg1) + '...';
         return text;
     }
 }
