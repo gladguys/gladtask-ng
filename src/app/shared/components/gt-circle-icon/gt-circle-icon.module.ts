50898f9aebfc6ac3c5b3e6e1ad7dd16444178fd2
Denis <dineresc@gmail.com>
2021-01-19 17:35:26 -0300

    add missing libs from fontawesome

--- src/app/shared/components/gt-circle-icon/gt-circle-icon.module.ts
+++ src/app/shared/components/gt-circle-icon/gt-circle-icon.module.ts
@@ -2,10 +2,17 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
 import { GTCircleIconComponent } from './gt-circle-icon.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 @NgModule({
   declarations: [GTCircleIconComponent],
-  imports: [MaterialModule, FlexLayoutModule],
+  imports: [MaterialModule, FlexLayoutModule, FontAwesomeModule],
   exports: [GTCircleIconComponent],
 })
-export class GTCircleIconModule {}
+export class GTCircleIconModule {
+  constructor() {
+    library.add(faCircle);
+  }
+}
